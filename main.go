4281ed668196964eeff5e3374ad5ad8c8a34dd0f
shantanuparte <shantanuparte23@gmail.com>
2026-09-24 15:48:45 +0530

    made a basic layout

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
-import "fmt"
-
+import (
+	"fmt"
+	"net"
+	"os"
+	"time"
+)
 
 func main() {
-	fmt.Println("Welcome to the port scanner
-	")
+
+
+	args := os.Args[2:]
+	timeout := time.Second*2
+
+	for _, port := range args {
+		link := fmt.Sprintf("%s:%s", os.Args[1], port)
+		conn, err := net.DialTimeout("tcp", link, timeout)
+		if err != nil {
+			fmt.Printf("Failed To connect: %s:%v\n", port, err)
+			continue
+		}
+
+		fmt.Printf("Connected: %s\n", port)
+		conn.Close()
+
+	}
+
 }
