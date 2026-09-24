559e68ef8cf57f9f28aeec493876543d7829dd52
shantanuparte <shantanuparte23@gmail.com>
2026-09-24 16:59:36 +0530

    converted it into a function for more readiblity and will add concurency

--- main.go
+++ main.go
@@ -9,21 +9,33 @@ import (
 
 func main() {
 
-
+	
+	if(len(os.Args)) < 3{
+		fmt.Println("Not enough arguments: <host name> <port> <port> ...")
+	}
 	args := os.Args[2:]
-	timeout := time.Second*2
+	address := os.Args[1]
 
-	for _, port := range args {
-		link := fmt.Sprintf("%s:%s", os.Args[1], port)
-		conn, err := net.DialTimeout("tcp", link, timeout)
-		if err != nil {
-			fmt.Printf("Failed To connect: %s:%v\n", port, err)
-			continue
-		}
-
-		fmt.Printf("Connected: %s\n", port)
-		conn.Close()
-
+	start := time.Now()
+	for _, a := range args{
+		portScanner(a,address)
 	}
 
+	timeGone := time.Since(start)
+	fmt.Printf("\n\nTotal Time: %v\n",timeGone)
+	
 }
+
+
+func portScanner(port string, address string){
+	timeout := time.Second*2
+	network := fmt.Sprintf("%s:%s",address,port)
+	conn, err := net.DialTimeout("tcp",network, timeout)
+	if err != nil{
+		fmt.Printf("PORT: %s\tError: %v\n",port,err)
+		return
+	}
+	fmt.Printf("PORT: %s\tConnection succesful\n",port)
+	conn.Close()
+}
+
